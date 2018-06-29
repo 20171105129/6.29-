c3c599fdb86924cdae95e4c7b38b9554a105785c
20171105129 <3038914421@qq.com>
2018-06-29 19:32:14 +0800

    Initial Commit

---
+++ 6.29 晚/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  6.29 晚
+//
+//  Created by yinzichao on 18/6/29.
+//  Copyright © 2018年 yinzichao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
